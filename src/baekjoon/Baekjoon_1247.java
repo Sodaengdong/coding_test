26a4e029eeffae20e65b7b4a4b4510aa9f7bf248
Sodaengdong <oisujin98@gmail.com>
2023-01-10 16:09:15 +0900

    백준 1247번 하는중

---
+++ src/baekjoon/Baekjoon_1247.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_1247 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int testcasenum = 3; // final로 선언된 변수는 항상 같은 값을 가짐
+
+
+    }
+
+}
+
