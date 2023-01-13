eb30e9f281d65352a8341827f369f82cdecdf898
Sodaengdong <oisujin98@gmail.com>
2023-01-13 09:18:39 +0900

    백준 1267번 정답

--- src/baekjoon/Baekjoon_1267.java
+++ src/baekjoon/Baekjoon_1267.java
@@ -8,20 +8,22 @@ public class Baekjoon_1267 {
     Scanner sc = new Scanner(System.in);
 
     int num = sc.nextInt();
-    int sum = 0;
+    int y = 0;
+    int m = 0;
 
     for (int i =0; i<num; i++) {
         int time = sc.nextInt();
-        sum += time;
-    }
+        y += (time/30)+1;
+        m += (time/60)+1;
 
-    int y = sum%30;
-    int m = sum%60;
+    }
 
     if (10*y<15*m) {
         System.out.println("Y " + 10*y);
     }else if (10*y>15*m) {
         System.out.println("M " + 15*m);
+    }else {
+        System.out.println("Y M " + 10*y);
     }
 
     }
