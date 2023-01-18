788bae56d3e2ac43c8cd7e3d9c4133835e82bfcd
Sodaengdong <oisujin98@gmail.com>
2023-01-18 15:29:06 +0900

    백준 2441번 정답

---
+++ src/baekjoon/Baekjoon_2460.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_2460 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i<10; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            sum -= a;
+            sum += b;
+
+            if (sum>max) max = sum;
+
+        }
+        System.out.println(max);
+    }
+}
