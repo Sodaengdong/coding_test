0fbf2b381c0171b148ccae9d3995c6b06f61bff9
Sodaengdong <oisujin98@gmail.com>
2023-02-06 10:01:35 +0900

    백준 11719번 정답

---
+++ src/baekjoon/Baekjoon_11719.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_11719 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String a = sc.nextLine();
+            System.out.println(a);
+        }
+    }
+}
