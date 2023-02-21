bca408cec2e2563f17a641188da4d0bec5acfec8
Sodaengdong <oisujin98@gmail.com>
2023-02-21 16:54:40 +0900

    백준 2577번 하는중

---
+++ src/baekjoon/Baekjoon_2577.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_2577 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arr[] = new int[10];
+        int sum = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        String str = Integer.toString(sum);
+
+
+
+
+
+    }
+}
