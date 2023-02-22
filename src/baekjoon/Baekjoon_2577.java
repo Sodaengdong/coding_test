da42515e32adf16b7bfa8955bbe0c1c5ff360c63
Sodaengdong <oisujin98@gmail.com>
2023-02-22 11:24:35 +0900

    백준 2577번 정답

--- src/baekjoon/Baekjoon_2577.java
+++ src/baekjoon/Baekjoon_2577.java
@@ -12,9 +12,18 @@ public class Baekjoon_2577 {
         int sum = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
         String str = Integer.toString(sum);
 
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < 10; j++) {
+                if (str.charAt(i) - '0' == j ) {
+                    ++arr[j];
+                    break;
+                }
+            }
+        }
 
-
-
-
+        for (int i = 0; i < 10; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
     }
 }
