72c0970bc0601e3faf61e26867ee6444ea7411cf
Sodaengdong <oisujin98@gmail.com>
2023-03-03 10:48:20 +0900

    백준 1292번 정답

--- src/baekjoon/Baekjoon_1292.java
+++ src/baekjoon/Baekjoon_1292.java
@@ -1,26 +1,27 @@
 package baekjoon;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Baekjoon_1292 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<Integer>();
 
         int a = sc.nextInt();
         int b = sc.nextInt();
+        int array[] = new int[b];
+        int index = 0;
         int sum = 0;
 
-        for (int i = 0; i < 1000; i++) {
-            for (int j = 0; j >= i; j++) {
-                list.add(i + 1);
+        for (int i = 1; i < b; i++) {
+            for (int j = 0; j < i; j++) {
+               if (index == b) break;
+               array[index++] = i;
             }
         }
 
-        for (int i = a-1; i < b; i++) {
-            sum += list.get(i);
+        for (int i = a; i <= b; i++) {
+            sum += array[i-1];
         }
 
         System.out.println(sum);
